538df4ca2a3f3208cbd0e50e8ca7dae2b65ac452
fuwilliam <william.fu@toggl.com>
2024-12-21 09:19:21 -0300

    AoSQL Day 21

---
+++ adventofsql2024/day21.sql
@@ -0,0 +1,27 @@
+WITH quarterly_sales AS
+(
+    SELECT
+        EXTRACT(YEAR FROM sale_date) AS sale_year,
+        EXTRACT(QUARTER FROM sale_date) AS sale_quarter,
+        SUM(amount) AS total_sales
+    FROM sales
+    GROUP BY 1, 2
+),
+
+qoq_sales AS
+(
+    SELECT
+        sale_year,
+        sale_quarter,
+        total_sales,
+        LAG(total_sales) OVER(ORDER BY sale_year, sale_quarter) AS last_quarter_total_sales
+    FROM quarterly_sales
+)
+
+SELECT
+    sale_year,
+    sale_quarter,
+    (total_sales / last_quarter_total_sales) - 1 AS growth_rate
+FROM qoq_sales
+WHERE last_quarter_total_sales IS NOT NULL
+ORDER BY 3 DESC
